d7e18f1c0bf2559c975e06009b3d5c84d74aa5bd
David Gilstad <david.gilstad@ou.edu>
2019-03-25 19:30:00 -0500

    Create and set up project 4

---
+++ Source.cpp
@@ -0,0 +1,32 @@
+/*
+ * Data Structures Project 4
+ *
+ * This project provides a class and main method for a tree data structures. SDG
+ * 
+ *			 author: David Gilstad
+ *			created: 25 March 2019
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * This class reperesents a tree data structure. A tree consists of nodes that hold
+ * information. Nodes have a parent and children that are connected to other nodes in the
+ * tree with branches. Nodes can have as many children as needed (or none), but all nodes
+ * have exactly one parent node, with the exception of the root node which has none.
+ */
+template <class T>
+class Tree {
+
+};
+
+/*
+ * Main method: Reads in information for creating a tree and prints out data using the
+ * methods in the tree class.
+ */
+int main() {
+
+
+	system("pause");
+	return 0;
+}
